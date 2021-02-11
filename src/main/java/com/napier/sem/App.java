b60e30b0166286e0c695e771ad489fb2196fdd94
NapierDanel <40511533@live.napier.ac.uk>
2021-02-11 17:14:53 +0100

    created DatabaseCall Method

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -10,17 +10,10 @@ public class App {
         // Create a new DatabaseLink class, containing connect and disconnect functionality, and database interactiond
         DatabaseLink db = new DatabaseLink();
 
-
         DatabaseLink.connInstance();
 
-        System.out.println("Looping through the first 500 cities within the database");
-
-
-        // Loop through the first 500 entries in the database
-        for(int i = 1; i < 500; i++)
-        {
-            System.out.println(db.getCity(i).name + " " + db.getCity(i).population);
-        }
+        Country country = new Country();
+        country.getCountriesByPopulationDESC();
 
 
         // Disconnect from database
