fe27fabef8d721dfbb8c5f23fa17977f351f433f
BeLeever <beaverl4006@gmail.com>
2021-02-26 10:43:02 +0000

    Added all the new methods within the city class, to provide the requiered functionality.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -10,21 +10,10 @@ public class App {
         // Create a new DatabaseLink class, containing connect and disconnect functionality, and database interactiond
         DatabaseLink db = new DatabaseLink();
 
-        City city = new City();
-
-
-
 
         DatabaseLink.connInstance();
 
         System.out.println("Looping through the first 500 cities within the database");
-
-
-        // Loop through the first 500 entries in the database
-        for(int i = 1; i < 500; i++)
-        {
-            System.out.println(city.getCity(i).name + " " + city.getCity(i).population);
-        }
 
 
         // Disconnect from database
--- src/main/java/com/napier/sem/City.java
+++ src/main/java/com/napier/sem/City.java
@@ -51,6 +51,240 @@ class City {
         }
     }
 
+    public ResultSet getAllCitiesInWorld(String limit)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = DatabaseLink.connInstance().createStatement();
+
+            int limitInt = Integer.parseInt(limit);
+
+            if(limitInt == 0)
+            {
+                limit = "";
+            }
+            else
+            {
+                limit = "LIMIT " + limitInt;
+
+            }
+
+
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT Name, Population "
+                            + "FROM city "
+                            + "ORDER BY Population DESC "
+                            + limit;
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            return rset;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get all city details");
+            return null;
+        }
+    }
+
+    public ResultSet getCitiesInContinent(String continent, String limit)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = DatabaseLink.connInstance().createStatement();
+
+            int limitInt = Integer.parseInt(limit);
+
+            if(limitInt == 0)
+            {
+                limit = "";
+            }
+            else
+            {
+                limit = "LIMIT " + limitInt;
+
+            }
+
+
+            // Create string for SQL statement
+            String strSelect =
+                    "Select City.Name, City.Population "
+                            + "FROM city "
+                            + "INNER JOIN Countries ON city.CountryCode = country.Code "
+                            + "WHERE country.continent = "
+                            + continent
+                            + " "
+                            + "ORDER BY city.Population DESC "
+                            + limit;
+
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+
+            return rset;
+
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get city details");
+            return null;
+        }
+    }
+
+    public ResultSet getCitiesInRegion(String region, String limit)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = DatabaseLink.connInstance().createStatement();
+
+            int limitInt = Integer.parseInt(limit);
+
+            if(limitInt == 0)
+            {
+                limit = "";
+            }
+            else
+            {
+                limit = "LIMIT " + limitInt;
+
+            }
+
+            // Create string for SQL statement
+            String strSelect =
+                    "Select City.Name, City.Population "
+                            + "FROM city "
+                            + "INNER JOIN Countries ON city.CountryCode = country.Code "
+                            + "WHERE country.Region = "
+                            + region
+                            + " "
+                            + "ORDER BY city.Population DESC"
+                            + limit;
+
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+
+            return rset;
+
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get city details");
+            return null;
+        }
+
+    }
+
+    public ResultSet getCitiesInCountry(String country, String limit)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = DatabaseLink.connInstance().createStatement();
+
+            int limitInt = Integer.parseInt(limit);
+
+            if(limitInt == 0)
+            {
+                limit = "";
+            }
+            else
+            {
+                limit = "LIMIT " + limitInt;
+
+            }
+
+
+            // Create string for SQL statement
+            String strSelect =
+                    "Select City.Name, City.Population "
+                            + "FROM city "
+                            + "INNER JOIN Countries ON city.CountryCode = country.Code "
+                            + "WHERE country.Name = "
+                            + country
+                            + " "
+                            + "ORDER BY city.Population DESC "
+                            + limit;
+
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+
+            return rset;
+
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get city details");
+            return null;
+        }
+
+    }
+
+    public ResultSet getCitiesInDistrict(String district, String limit)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = DatabaseLink.connInstance().createStatement();
+
+            int limitInt = Integer.parseInt(limit);
+
+            if(limitInt == 0)
+            {
+                limit = "";
+            }
+            else
+            {
+                limit = "LIMIT " + limitInt;
+
+            }
+
+
+            // Create string for SQL statement
+            String strSelect =
+                    "Select Name, Population "
+                            + "FROM city "
+                            + "WHERE district = "
+                            + district
+                            + " "
+                            + "ORDER BY Population DESC "
+                            + limit;
+
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+
+            return rset;
+
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get city details");
+            return null;
+        }
+
+    }
+
+
+
+
+
+
 
 
 
