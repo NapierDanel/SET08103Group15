04ffb8024e5780ed016a77c231a0d582f39264ba
BeLeever <beaverl4006@gmail.com>
2021-03-01 19:46:52 +0000

    Added all the javadoc comment for App class

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -2,21 +2,30 @@ package com.napier.sem;
 
 import java.sql.*;
 
+/**
+ * The main class where all other classes are instantiated
+ */
 public class App {
 
+    /**
+     * The main instantiates the main classes and creates a link to the database
+     * @param args     Currently not in use
+     */
     public static void main(String[] args)
     {
 
-        // Create a new DatabaseLink class, containing connect and disconnect functionality, and database interactiond
+        // Create a new DatabaseLink class, containing connect and disconnect functionality, and database interactions
         DatabaseLink db = new DatabaseLink();
 
-
+        // Connects to the database
         DatabaseLink.connInstance();
 
-        System.out.println("Looping through the first 500 cities within the database");
+
 
         // Disconnect from database
         db.disconnect();
+
+        System.out.println("Disconnected from the database");
 
 
     }
